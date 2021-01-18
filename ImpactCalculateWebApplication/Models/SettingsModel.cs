9b9794f9f48bada42046f0287a5573e36a41d74e
Егор <egor.demyanenko2000@mail.ru>
2021-01-18 15:56:45 +0300

    Settings serialization

---
+++ ImpactCalculateWebApplication/Models/SettingsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImpactCalculateWebApplication.Models
+{
+    public class SettingsModel
+    {
+        public double L1 { get; set; } 
+        public double L2 { get; set; } 
+        public double S1 { get; set; } 
+        public double S2 { get; set; } 
+        public double Wgr { get; set; }
+
+        public string SelectedCocks { get; set; }
+    }
+}
