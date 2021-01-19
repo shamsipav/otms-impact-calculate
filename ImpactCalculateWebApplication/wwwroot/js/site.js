13554c80cac6d3a0f35351c94990db4f1d4ae35f
SHAMSI <aseevpav@gmail.com>
2021-01-20 03:30:05 +0500

    LAST COMMIT 19.01

--- ImpactCalculateWebApplication/wwwroot/js/site.js
+++ ImpactCalculateWebApplication/wwwroot/js/site.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
         $("#AddRow").click(function () {
             let rowCount = $('#table tbody tr').length;
             $("#table").scrollTop($("#table")[0].scrollHeight);
+            SetEvenClassOnTable("#table tbody tr");
 
             if (rowCount > 4) {
                 $(".table").addClass("overflow-y");
@@ -27,20 +28,95 @@ $(document).ready(function () {
         })
     }
 
-    //$("#AddRow").click(function () {
+    //for (let i = 0; i < $(".table").length; i++) {
+    //    let tableID = "#" + $(".table").eq(i).attr("id");
 
-    //    //let block = document.querySelector(".table");
+    //    console.log(tableID);
 
+    //    SetEvenClassOnTable(tableID);
+    //}
 
-    //    $(".table").scrollTop($(".table")[0].scrollHeight);
+    //for (let i = 0) {
 
-    //    if ($(".table").height() > 400) {
-    //        $(".table").addClass("overflow-y");
+    //}
+
+    SetEvenClassOnTable("#table");
+    SetEvenClassOnTable("#gas-table");
+    SetEvenClassOnTable("#device-table");
+    SetEvenClassOnTable("#gas-device-difference-table");
+    SetEvenClassOnTable("#heat-content-table");
+    SetEvenClassOnTable("#material-balance-coming-table");
+    SetEvenClassOnTable("#material-balance-consumption-table");
+    SetEvenClassOnTable("#w-table");
+    SetEvenClassOnTable("#heat-balance-coming-table");
+    SetEvenClassOnTable("#heat-balance-consumption-table");
+    SetEvenClassOnTable("#heat-balance-consumption-table");
+    SetEvenClassOnTable("#oxides-percent-table");
+
+    function SetEvenClassOnTable(table) {
+        let rowCount = $(`${table} tbody tr`).length;
+
+        for (let i = 0; i < rowCount; i++) {
+            if (i % 12 == 0) {
+                let rowSpanValue = (i / 12) + 1;
+
+                if (rowSpanValue % 2 == 0) {
+                    $(`${table} tbody tr td:first-child`).eq(i).addClass("even-row");
+                }
+            }
+        }
+    }
+
+    //let rowCount = $(`${table} tbody tr`).length;
+
+    //for (let i = 0; i < rowCount; i++) {
+    //    if (i % 12 == 0) {
+    //        let rowSpanValue = (i / 12) + 1;
+
+    //        if (rowSpanValue % 2 == 0) {
+    //            for (let j = 0; j < 12; j++) {
+    //                $(this).addClass("test");
+    //                $(`${table} tbody tr`).eq(i + j).addClass("even-row");
+    //            }
+    //        }
     //    }
-    //})
+    //}
 
-    //$(window).scroll(function () {
-    //    $(".head").css("height", 200 + "px");
-    //})
+
+    $('.table-result').hover(function () {
+        $('.table-result').addClass("no-scroll");
+        $(this).removeClass("no-scroll");
+        //console.log($(this).attr("id"));
+    });
+
+    $(".table-result").on('scroll', function () {
+        let gavno = $(this).scrollTop();
+        $(".table-result.no-scroll").scrollTop(gavno);       
+    })
+
+    let kiloPerHour = true;
+    let percent = false;
+
+    $("#materialBalance").click(function () {
+        console.log("click")
+        kiloPerHour = !kiloPerHour;
+
+        if (kiloPerHour) {
+            $("#materialBalance").text("Показать на тонну расплава");
+            $(".material-balance-heading").text("Материальный баланс (приход), кг/ч");
+            $("#material-balance-coming-table tbody tr.material-balance-tonn").css("display", "none");
+            $("#material-balance-coming-table tbody tr.material-balance-hour").css("display", "table-row");
+        } else {
+            $("#materialBalance").text("Показать в час");
+            $(".material-balance-heading").text("Материальный баланс (приход), кг/т");
+            $("#material-balance-coming-table tbody tr.material-balance-hour").css("display", "none");
+            $("#material-balance-coming-table tbody tr.material-balance-tonn").css("display", "table-row");
+        }
+    });
+
+    $("#percent").click(function () {
+
+        percent = !percent;
+    });
 
 })
