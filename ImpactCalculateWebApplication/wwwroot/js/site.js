8a5a3752d5671f6e8bd32b5874768f5950b475ea
SHAMSI <aseevpav@gmail.com>
2021-01-20 16:43:16 +0500

    FIRST COMMIT 20.01

--- ImpactCalculateWebApplication/wwwroot/js/site.js
+++ ImpactCalculateWebApplication/wwwroot/js/site.js
@@ -45,11 +45,21 @@ $(document).ready(function () {
     SetEvenClassOnTable("#device-table");
     SetEvenClassOnTable("#gas-device-difference-table");
     SetEvenClassOnTable("#heat-content-table");
+
     SetEvenClassOnTable("#material-balance-coming-table");
-    SetEvenClassOnTable("#material-balance-consumption-table");
+
+    SetEvenClassOnTable("#material-balance-coming-table-hour");
+    SetEvenClassOnTable("#material-balance-coming-table-tonn");
+
+    SetEvenClassOnTable("#material-balance-coming-table-percent-hour");
+
+    SetEvenClassOnTable("#material-balance-consumption-table-hour");
+    SetEvenClassOnTable("#material-balance-consumption-table-tonn");
+    //SetEvenClassOnTable("#material-balance-consumption-table-percent");
+
     SetEvenClassOnTable("#w-table");
+
     SetEvenClassOnTable("#heat-balance-coming-table");
-    SetEvenClassOnTable("#heat-balance-consumption-table");
     SetEvenClassOnTable("#heat-balance-consumption-table");
     SetEvenClassOnTable("#oxides-percent-table");
 
@@ -61,7 +71,10 @@ $(document).ready(function () {
                 let rowSpanValue = (i / 12) + 1;
 
                 if (rowSpanValue % 2 == 0) {
-                    $(`${table} tbody tr td:first-child`).eq(i).addClass("even-row");
+                    for (let j = 0; j < 12; j++) {
+                        $(this).addClass("test");
+                        $(`${table} tbody tr`).eq(i + j).addClass("even-row");
+                    }
                 }
             }
         }
@@ -74,15 +87,12 @@ $(document).ready(function () {
     //        let rowSpanValue = (i / 12) + 1;
 
     //        if (rowSpanValue % 2 == 0) {
-    //            for (let j = 0; j < 12; j++) {
-    //                $(this).addClass("test");
-    //                $(`${table} tbody tr`).eq(i + j).addClass("even-row");
-    //            }
+    //            $(`${table} tbody tr td:first-child`).eq(i).addClass("even-row");
     //        }
     //    }
     //}
 
-
+ 
     $('.table-result').hover(function () {
         $('.table-result').addClass("no-scroll");
         $(this).removeClass("no-scroll");
@@ -94,29 +104,51 @@ $(document).ready(function () {
         $(".table-result.no-scroll").scrollTop(gavno);       
     })
 
-    let kiloPerHour = true;
-    let percent = false;
+    //
 
-    $("#materialBalance").click(function () {
+    let kiloPerHour = true;
+
+    $("#balanceConfigure").click(function () {
         console.log("click")
         kiloPerHour = !kiloPerHour;
 
+        $(".table-result").scrollTop(0);
+
         if (kiloPerHour) {
-            $("#materialBalance").text("Показать на тонну расплава");
-            $(".material-balance-heading").text("Материальный баланс (приход), кг/ч");
-            $("#material-balance-coming-table tbody tr.material-balance-tonn").css("display", "none");
-            $("#material-balance-coming-table tbody tr.material-balance-hour").css("display", "table-row");
+            $("#balanceConfigure").text("Показать на тонну расплава");
+            $(".material-balance-coming-heading").text("Материальный баланс (приход), кг/ч");
+            $(".material-balance-consumption-heading").text("Материальный баланс (расход), кг/ч");
+
+            $(".table-balance-tonn").addClass("table-hide");
+            $(".table-balance-hour").removeClass("table-hide");
         } else {
-            $("#materialBalance").text("Показать в час");
-            $(".material-balance-heading").text("Материальный баланс (приход), кг/т");
-            $("#material-balance-coming-table tbody tr.material-balance-hour").css("display", "none");
-            $("#material-balance-coming-table tbody tr.material-balance-tonn").css("display", "table-row");
+            $("#balanceConfigure").text("Показать в час");
+            $(".material-balance-coming-heading").text("Материальный баланс (приход), кг/т");
+            $(".material-balance-consumption-heading").text("Материальный баланс (расход), кг/т");
+
+            $(".table-balance-hour").addClass("table-hide");
+            $(".table-balance-tonn").removeClass("table-hide");
         }
     });
+
+    //
+
+    let percent = false;
 
     $("#percent").click(function () {
 
         percent = !percent;
+
+        if (kiloPerHour && percent) {
+            $(".material-balance-coming-heading").text("Материальный баланс (приход), % (кг/ч)");
+
+            $(".table-balance-tonn").addClass("table-hide");
+            $(".table-balance-hour").addClass("table-hide");
+
+            $(".table-balance-percent-hour").removeClass("table-hide");
+        } else {
+
+        }
     });
 
 })
